bd9b9d68c2c89036a208dec684afbb4eab664b20
sunicyosen <sunicyosen@outlook.com>
2019-05-07 12:03:50 +0800

    add rvsim set parameters

--- rvSim/rvsim_set_parameters.cc
+++ rvSim/rvsim_set_parameters.cc
@@ -10,21 +10,6 @@
 #include <stdlib.h>
 #include <fstream>
 
-#include "src/Interconnect.h"
-#include "Interconnect/InterconnectFactory.h"
-#include "src/Config.h"
-#include "src/TranslationMethod.h"
-#include "traceReader/TraceReaderFactory.h"
-#include "src/AddressTranslator.h"
-#include "Decoders/DecoderFactory.h"
-#include "src/MemoryController.h"
-#include "MemControl/MemoryControllerFactory.h"
-#include "Endurance/EnduranceDistributionFactory.h"
-#include "SimInterface/NullInterface/NullInterface.h"
-#include "include/NVMHelpers.h"
-#include "Utils/HookFactory.h"
-#include "src/EventQueue.h"
-#include "NVM/nvmain.h"
 #include "rvSim/rvSim.h"
 
 using namespace NVM;
@@ -32,6 +17,7 @@ using namespace NVM;
 //Define some params
 extern rvSim *riscv_sim;
 
-extern "C" int rvsim_set_parameters(){
+extern "C" int rvsim_set_parameters()
+{
   riscv_sim->setParameters();
 }
