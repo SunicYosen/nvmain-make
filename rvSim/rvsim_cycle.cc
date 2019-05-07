5be586e9664dc8e84c51f1ee6a1275f9646a7193
sunicyosen <sunicyosen@outlook.com>
2019-05-07 12:00:24 +0800

    modified rvsim cycle

--- rvSim/rvsim_cycle.cc
+++ rvSim/rvsim_cycle.cc
@@ -10,21 +10,6 @@
 #include <stdlib.h>
 #include <fstream>
 
-#include "src/Interconnect.h"
-#include "Interconnect/InterconnectFactory.h"
-#include "src/Config.h"
-#include "src/TranslationMethod.h"
-#include "traceReader/TraceReaderFactory.h"
-#include "src/AddressTranslator.h"
-#include "Decoders/DecoderFactory.h"
-#include "src/MemoryController.h"
-#include "MemControl/MemoryControllerFactory.h"
-#include "Endurance/EnduranceDistributionFactory.h"
-#include "SimInterface/NullInterface/NullInterface.h"
-#include "include/NVMHelpers.h"
-#include "Utils/HookFactory.h"
-#include "src/EventQueue.h"
-#include "NVM/nvmain.h"
 #include "rvSim/rvSim.h"
 
 using namespace NVM;
