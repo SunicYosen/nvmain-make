74d7374e40207f7c712054f96da9f7fe939d9069
sunicyosen <sunicyosen@outlook.com>
2019-05-14 01:05:27 +0800

    Add define of some function

--- rvSim/rvSim.h
+++ rvSim/rvSim.h
@@ -13,23 +13,25 @@ namespace NVM {
       rvSim( );
       ~rvSim( );
 
-      void SetConfig(int argc, char *argv[]);
+    void SetConfig(int argc, char *argv[]);
 
-      void Cycle( ncycle_t steps );
+    void Cycle( ncycle_t steps );
 
-      NVMainRequest *linetocommand( uint64_t addr, char opt, uint64_t data, uint64_t threadId );
-      bool IsIssuable( uint64_t input_addr, uint64_t output_addr, char opt, uint64_t data, char slide);
-      bool IsIssuable( uint64_t addr, char opt, uint64_t data, uint64_t threadId );
-      bool IsIssuable( NVMainRequest *request, FailReason * fail=NULL);
-      bool IssueCommand( uint64_t input_addr, uint64_t output_addr, char opt, uint64_t data, char slide);
-      bool IssueCommand( uint64_t addr, char opt, uint64_t data, uint64_t threadId );
-      bool IssueCommand( NVMainRequest * request );
-      bool RequestComplete( NVMainRequest *request );
-      
-      bool setParameters();
-      
-      uint64_t getCycle();
-      
+  	NVMainRequest *linetocommand( uint64_t addr, char opt, uint64_t data, uint64_t threadId );
+    bool IsIssuable( uint64_t input_addr, uint64_t output_addr, char opt, uint64_t data, char slide);
+  	bool IsIssuable( uint64_t addr, char opt, uint64_t data, uint64_t threadId );
+    bool IsIssuable( NVMainRequest *request, FailReason * fail=NULL);
+    bool IsIssuable( );
+    bool IssueCommand( uint64_t input_addr, uint64_t output_addr, char opt, uint64_t data, char slide);
+    bool IssueCommand( uint64_t addr, char opt, uint64_t data, uint64_t threadId, char transfer_mode, uint64_t transfer_size);
+  	bool IssueCommand( uint64_t addr, char opt, uint64_t data, uint64_t threadId );
+  	bool IssueCommand( NVMainRequest * request );
+    bool RequestComplete( NVMainRequest *request );
+    
+    bool setParameters();
+    
+    uint64_t getCycle();
+    
     protected:
     
     private:
@@ -44,6 +46,8 @@ namespace NVM {
       EventQueue *mainEventQueue ;
       GlobalEventQueue *globalEventQueue ;
       TagGenerator *tagGenerator ;
+      std::list<NVMainRequest *> CommandQueue;
+      uint64_t CommandQueueSize;
     
       uint64_t simulateCycles;
       uint64_t currentCycle;
