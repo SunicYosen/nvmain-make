05d20b2d0a8f2503f7d279622633cf272465e1ff
sunicyosen <sunicyosen@outlook.com>
2019-05-24 22:47:12 +0800

    Add act/pool drive function

---
+++ rvSim/rvsim_pool_drive.cc
@@ -0,0 +1,27 @@
+/* 
+ * Name:  rvsim_pool_drive.cc
+ * Author:  SunicYosen
+ * Time:  2019.04.11
+ * Introduction:
+ */
+
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <stdlib.h>
+#include <fstream>
+#include "stdio.h"
+
+#include "vpi_user.h"
+#include "acc_user.h"
+#include "rvSim/rvSim.h"
+
+using namespace NVM;
+
+//Define some params
+extern rvSim *riscv_sim;
+
+extern "C" int rvsim_pool_drive()
+{
+  //TODO
+}
