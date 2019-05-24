cd82669bb7c3bd39494e225d01b3b2f5e4192722
sunicyosen <sunicyosen@outlook.com>
2019-05-24 21:24:43 +0800

    Modiied set func function

--- rvSim/rvsim_set_func.cc
+++ rvSim/rvsim_set_func.cc
@@ -1,7 +1,7 @@
 /* 
- * Name:  rvsim_set_config.cc
+ * Name:  rvsim_set_func.cc
  * Author:  SunicYosen
- * Time:  2019.04.11
+ * Time:    2019.05.24
  * Introduction:
  */
 
@@ -26,37 +26,56 @@ extern GlobalParams globalparams;
 extern "C" int rvsim_set_func()
 {
   vpiHandle sysTfH, argI;
-  vpiHandle argv[2];
-  s_vpi_value argv_val[2];
+  vpiHandle argv[5];
+  s_vpi_value argv_val[5];
 
   PLI_UINT64 value_arg0;
+  PLI_UINT64 value_arg1;
+  PLI_UINT64 value_arg2;
+  PLI_UINT64 value_arg3;
 
   argv_val[0].format = vpiIntVal;
+  argv_val[1].format = vpiIntVal;
+  argv_val[2].format = vpiIntVal;
+  argv_val[3].format = vpiIntVal;
 
   sysTfH = vpi_handle(vpiSysTfCall, NULL);
   argI = vpi_iterate(vpiArgument, sysTfH);
 
-  for (int i = 0; i < 1; i++)
+  for (int i = 0; i < 4; i++)
   {
      argv[i] = vpi_scan(argI);
      vpi_get_value(argv[i], &argv_val[i]);
   }
 
   value_arg0 = argv_val[0].value.integer;
+  value_arg1 = argv_val[1].value.integer;
+  value_arg2 = argv_val[2].value.integer;
+  value_arg3 = argv_val[3].value.integer;
 
   vpi_free_object(argI);
 
   if(!globalparams.isUsing)
 	{
-    uint64_t func_value = value_arg0;
-    printf("[+](set_func) Func_n = %d\n", func_value);
-		globalparams.Func_n = 0;
+
+    uint64_t input_col      = value_arg0;
+    uint64_t input_row      = value_arg1;
+    uint64_t input_channels = value_arg2;
+    uint64_t input_bitwidth = value_arg3;
+
+    printf("[+](set_input) Input col=%d row=%d channels=%d bitwidth=%d\n", \
+                          input_col, input_row, input_channels, input_bitwidth);
+
+		globalparams.Input_Col     = input_col;
+    globalparams.Input_Row     = input_row;
+    globalparams.Input_Channel = input_channels;
+    globalparams.Input_Width   = input_bitwidth;
 		return true;
-	}
+	}  
 
 	else
   {
-    printf("[-](set_func) Failed: globalparams is using!\n");
+    printf("[-](set_input) Failed: globalparams is using!\n");
     return false;
   }
 }
